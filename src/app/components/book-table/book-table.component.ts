c79b72434c705f69ab18e2fee8e3b88896094cab
john_guerrero1 <johnalexguerrero@gmail.com>
2025-08-24 00:00:33 -0500

    init component: bookTableComponent

---
+++ src/app/components/book-table/book-table.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+
+@Component({
+  selector: 'app-book-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-table.component.html',
+  styleUrl: './book-table.component.css'
+})
+export class BookTableComponent implements OnInit{
+  dataset: any[] = [];
+  
+  constructor(
+    private bookService: BookService
+  ){}
+
+  ngOnInit(): void {
+    this.bookService.getAllEbooks().then(
+      res => this.dataset = res
+    );
+  }
+
+}
